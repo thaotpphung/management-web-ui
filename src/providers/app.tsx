ddea825c4600de48df5cac6c6129b5b577aa7d3b
Thao Phung <thao.tp.phung@gmail.com>
2023-10-08 12:03:54 -0400

    1: Update setup

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { HelmetProvider } from 'react-helmet-async';
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { BrowserRouter as Router } from 'react-router-dom';
+
+import { Button, Spinner } from '@/components/Elements';
+import { Notifications } from '@/components/Notifications/Notifications';
+import { AuthProvider } from '@/lib/auth';
+import { queryClient } from '@/lib/react-query';
 
 const ErrorFallback = () => {
   return (
@@ -10,6 +17,9 @@ const ErrorFallback = () => {
       role="alert"
     >
       <h2 className="text-lg font-semibold">Ooops, something went wrong :( </h2>
+      <Button className="mt-4" onClick={() => window.location.assign(window.location.origin)}>
+        Refresh
+      </Button>
     </div>
   );
 };
@@ -21,11 +31,21 @@ type AppProviderProps = {
 export const AppProvider = ({ children }: AppProviderProps) => {
   return (
     <React.Suspense
-      fallback={<div className="flex items-center justify-center w-screen h-screen"></div>}
+      fallback={
+        <div className="flex items-center justify-center w-screen h-screen">
+          <Spinner size="xl" />
+        </div>
+      }
     >
       <ErrorBoundary FallbackComponent={ErrorFallback}>
         <HelmetProvider>
-          <Router>{children}</Router>
+          <QueryClientProvider client={queryClient}>
+            {process.env.NODE_ENV !== 'test' && <ReactQueryDevtools />}
+            <Notifications />
+            <AuthProvider>
+              <Router>{children}</Router>
+            </AuthProvider>
+          </QueryClientProvider>
         </HelmetProvider>
       </ErrorBoundary>
     </React.Suspense>
